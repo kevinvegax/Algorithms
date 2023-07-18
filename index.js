b726a37e104b176620b467573e812359b3f7a8c6
kevin <ing.kevinvega23@gmail.com>
2023-07-17 19:24:42 -0600

    Linear and Binary Search

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 // Linear Search Algorithm
 
 function linearSearch(arr, key){
-    for(i = 0; i < arr.length; i++){
-        if(arr[i] == key){
-            return i
-        }
+  for(i = 0; i < arr.length; i++){
+    if(arr[i] == key){
+      return i
     }
+  }
 
-    return 'Index not found'
+  return 'Index not found'
 }
 
 console.log(linearSearch([1, 2, 3, 4], 4))
 console.log(linearSearch([1, 2, 3, 4], 5))
 
 // Binary Search algorithm
+
+function recursiveFunction(arr, key){
+  let left = 0;
+  let right = arr.length;
+
+  while(left <= right){
+    const mid = Math.floor((left+right)/2)
+
+    if(arr[mid] === key){
+      return mid
+    } else if(arr[mid] < key){
+      left = mid+1
+    } else{
+      right = mid - 1
+    }
+  }
+
+  return 'Value not found'
+}
+
+console.log(recursiveFunction([1, 2, 3, 4, 5, 6, 7], 7))
+console.log(recursiveFunction([1, 2, 3, 4, 5, 6, 7], 8))
